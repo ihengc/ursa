59874ffb511380229786d338000d0b79fa1567e1
ihengc <ihengc@163.com>
2022-06-02 17:37:06 +0800

    create message struct

--- gateway/message.go
+++ gateway/message.go
@@ -5,4 +5,32 @@ package gateway
 * @date: 2022/6/2 0002 17:19
 * @version: 1.0
 * @description:
+request message length: 4 + 4 + 8 + 1 + 1 + 1 + len(data) = 19 + len(data)
+response message length: 4 = 4 + 8 + 1 + 1 + 1 + 4 + len(data) = 23 + len(data)
 *********************************************************/
+
+const (
+	RequestMessageHeaderSize  = 19
+	ResponseMessageHeaderSize = 23
+)
+
+type RequestMessage struct {
+	id            int32
+	sequenceId    int32
+	sendTimestamp int64
+	isCompressed  bool
+	isEncrypted   bool
+	serviceType   byte
+	data          []byte
+}
+
+type ResponseMessage struct {
+	id            int32
+	sequenceId    int32
+	sendTimestamp int64
+	isCompressed  bool
+	isEncrypted   bool
+	serviceType   byte
+	errorCode     int32
+	data          []byte
+}
