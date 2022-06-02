0f32202531ef7b59658068b480781b384fa7930c
ihengc <ihengc@163.com>
2022-06-02 18:18:36 +0800

    create serialization struct

---
+++ gateway/serialize/protobuf/serialization.go
@@ -0,0 +1,18 @@
+package protobuf
+
+import "ursa/gateway"
+
+/********************************************************
+* @author: Ihc
+* @date: 2022/6/2 0002 17:30
+* @version: 1.0
+* @description:
+*********************************************************/
+
+func Serialize(responseMessage *gateway.ResponseMessage) []byte {
+	panic("")
+}
+
+func Deserialize(requestData []byte) *gateway.RequestMessage {
+	panic("")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module ursa
+
+go 1.18
